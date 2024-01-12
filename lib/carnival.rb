5cf76f7df1178d0778c0477f66ca475e35fc2ab9
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-11 22:17:03 -0700

    feat: carnival summary and unique visitors

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -22,4 +22,27 @@ class Carnival
   def total_revenue
     rides.sum { |ride| ride.total_revenue }
   end
+
+  def unique_visitors
+    results = {}
+
+    rides.each do |ride|
+      ride.rider_log.each do |visitor, ridden|
+        if !results.has_key?(visitor)
+          results[visitor] = "visited"
+        end
+      end
+    end
+
+    results.keys
+  end
+
+  def summary
+    {
+      visitor_count: unique_visitors.length,
+      revenue_earned: total_revenue,
+      visitors: unique_visitors,
+      rides: @rides
+    }
+  end
 end
